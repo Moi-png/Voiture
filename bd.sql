5e3d282b071cdc43c2df82d2ee5e4c23d1757618
ELPHILA <elphila@gmail.com>
2025-04-03 14:57:40 +0200

    bdsql

---
+++ bd.sql
@@ -0,0 +1,29 @@
+pragma encoding="UTF-8";
+
+create table users (
+  id integer primary key,
+  email text unique not null,
+  pseudo text unique not null,
+  password text not null
+);
+
+create table voiture (
+    id integer primary key,
+    signal integer not null,
+    hauteur integer not null,
+    largeur integer not null,
+    longueur integer not null,
+    nom text unique not null,
+    pmax integer not null,
+    cylindre integer not null,
+    mcouple integer not null,
+    transmission integer not null,
+    boite integer not null,
+    moteur integer not null,
+    posmoteur text not null,
+    energie integer not null,
+    vmax integer not null,
+    massevide integer not null,
+    marque text not null,
+    lienimage text not null
+);
